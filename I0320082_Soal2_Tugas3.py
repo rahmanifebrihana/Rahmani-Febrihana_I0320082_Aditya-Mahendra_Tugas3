fd36cd5f5cbc442693fa88adc88d82aecccda915
rahmani <rfebrihana@gmail.com>
2021-03-18 20:24:52 -0800

    Pengerjaan Soal

---
+++ I0320082_Soal2_Tugas3.py
@@ -0,0 +1,16 @@
+# Membuat Dictionary
+dict = {'Nama':'Rahmani Febrihana', 'Hobi 1':'Kulineran', 'Hobi 2':'Menonton Drama', 'Hobi 3':'Jalan Jalan', 'Sosial Media 1':'Intagram : Hannafbr', 'Sosial Media 2':'Line: Hannafbr', 'Sosial Media 3':'Twitter : febhanaa', 'Lagu Kesukaan 1':'Day&Night', 'Lagu Kesukaan 2':'If u could see me cryin in my room', 'Lagu Kesukaan 3':'Blue Jeans', 'Makanan Favorite 1':'Spaghetti', 'Makanan Favorite 2':'Sate Ayam', 'Makanan Favorite 3':'Tempe Geprek'}
+
+# Mengubah satu hobi dan sosial media
+dict['Hobi 2'] = 'Mendengarkan Musik'
+dict['Sosial Media 1'] = 'Instagram:hannavr.mell'
+
+# Menghapus 2 makanan favorite
+del dict['Makanan Favorite 1']
+del dict['Makanan Favorite 2']
+
+# Menambahkan 1 hobi
+dict['Hobi 4'] = 'Berkebun'
+
+print(dict)
+
